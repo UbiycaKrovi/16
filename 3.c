d96e142d57c6a71f039d75952ae144ce16205590
Ubiyca_Krovi <dimalumar2@gmail.com>
2024-04-23 11:13:51 +0300

    3

---
+++ 3.c
@@ -0,0 +1,27 @@
+#include "matrix.h"
+
+int getMin(int *a, int n) {
+    int min = a[0];
+    for (int i = 1; i < n; i++)
+        if (a[i] < min)
+            min = a[i];
+    return min;
+}
+
+void sortRowsByMinElement(matrix m) {
+    insertionSortColsMatrixByColCriteria(m, getMin);
+}
+
+int main() {
+    int nRows, nCols;
+    scanf("%d %d", &nRows, &nCols);
+    
+    matrix m = getMemMatrix(nRows, nCols);
+    inputMatrix(&m);
+    
+    sortRowsByMinElement(m);
+    
+    outputMatrix(m);
+    
+    return 0;
+}
