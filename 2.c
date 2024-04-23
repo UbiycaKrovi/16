e4c493fe2b4f8c9e8ee9bd2e5ae5d2a4e6b71f5b
Ubiyca_Krovi <dimalumar2@gmail.com>
2024-04-23 10:53:21 +0300

    2

---
+++ 2.c
@@ -0,0 +1,27 @@
+#include "matrix.h"
+
+int getMax(int *a, int n) {
+    int max = a[0];
+    for (int i = 1; i < n; i++)
+        if (a[i] > max)
+            max = a[i];
+    return max;
+}
+
+void sortRowsByMinElement(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
+}
+
+int main() {
+    int nRows, nCols;
+    scanf("%d %d", &nRows, &nCols);
+
+    matrix m = getMemMatrix(nRows, nCols);
+    inputMatrix(&m);
+
+    sortRowsByMinElement(m);
+
+    outputMatrix(m);
+    
+    return 0;
+}
